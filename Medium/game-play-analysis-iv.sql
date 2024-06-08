062eabf6c86023e20225d765014ad3cb15c727e5
tejanallagorla <nallagorlat@gmail.com>
2024-06-08 12:18:25 -0400

    day 5

---
+++ Medium/game-play-analysis-iv.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ROUND(COUNT(*) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM
+    Activity
+WHERE
+    (player_id , event_date) IN (
+		SELECT 
+            player_id,
+			DATE_ADD(MIN(event_date), INTERVAL 1 DAY)
+        FROM
+            Activity
+        GROUP BY player_id);
